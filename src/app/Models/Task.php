659315af858852f62d48968579bec8a8b60e07e5
Alocvetic <sprozorova92@gmail.com>
2024-07-14 01:27:29 +0700

    task: create models, migrations

---
+++ src/app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'task_status_id',
+        'deadline_date',
+    ];
+
+    public function taskStatus(): BelongsTo
+    {
+        return $this->belongsTo(TaskStatus::class);
+    }
+}
